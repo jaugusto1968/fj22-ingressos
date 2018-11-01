0e595a5a079499966a88edf6f7448cd12db8131e
Augusto <jag1968br-github@yahoo.com>
2018-11-01 13:41:56 -0300

    Exercício 5.5 Testanto a verificação de lugares disponíveis

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -97,7 +97,15 @@ public class Sessao {
 	}
 	
 	public boolean isDisponivel(Lugar lugarSelecionado){
-		return ingressos.stream().map(Ingresso::getLugar).noneMatch(lugar -> lugar.equals(lugarSelecionado));
+		return getIngressos().stream().map(Ingresso::getLugar).noneMatch(lugar -> lugar.equals(lugarSelecionado));
+	}
+
+	public Set<Ingresso> getIngressos() {
+		return ingressos;
+	}
+
+	public void setIngressos(Set<Ingresso> ingressos) {
+		this.ingressos = ingressos;
 	}
 	
 }
